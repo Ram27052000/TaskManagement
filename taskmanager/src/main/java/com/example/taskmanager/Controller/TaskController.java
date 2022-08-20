a86329108639eea2c11e39a0402842409baf633c
Ram27052000 <rvr1743@gmail.com>
2022-08-20 19:43:33 +0530

    Added SwaggerApi

--- taskmanager/src/main/java/com/example/taskmanager/Controller/TaskController.java
+++ taskmanager/src/main/java/com/example/taskmanager/Controller/TaskController.java
@@ -2,6 +2,7 @@ package com.example.taskmanager.Controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -19,6 +21,7 @@ import com.example.taskmanager.Repository.TaskRepository;
 import com.example.taskmanager.Service.TaskService;
 
 @RestController
+
 public class TaskController {
 
 		private TaskService taskservice;
@@ -30,43 +33,44 @@ public class TaskController {
 				super();
 				this.taskservice = taskservice;
 	    }
-		@PostMapping("/taskdetails")
+		
+		@RequestMapping(value="/taskdetails",method = RequestMethod.POST)
 		public String showMessage(@RequestBody Task task)
 		{
 			taskservice.listoftask(task);
 			return "success";
-		}	
-		@GetMapping("/showalltask")
+		}
+		@RequestMapping(value="/showalltask",method = RequestMethod.GET)
 		public List<Task> showall()
 		{
 			return taskservice.findall();
 			
 		}
-		@GetMapping("/showbyid/{id}")
+		@RequestMapping(value="/showbyid/{id}",method = RequestMethod.GET)
 		public Task showbyID(@PathVariable ("id") int id )
 		{
 			return taskservice.getbyId(id);
 		}
-		@PutMapping("/updatebyid/{id}")
+		@RequestMapping(value="/updatebyid/{id}",method = RequestMethod.PUT)
 		public Task updatebyID(@RequestBody Task task,@PathVariable("id")int id)
 		{
 			task.taskid=id;
 			return taskservice.listoftask(task);
 		}
 		
-		@GetMapping("/status/{status}")
+		@RequestMapping(value="/status/{status}",method = RequestMethod.GET)
 		public List<Task> gettaskbystatus(@PathVariable("status")String status)
 		{
 			
 			return taskrepository.gettaskbystatus(status);
 		}
-		@GetMapping("/priority/{priority}")
+		@RequestMapping(value="/priority/{priority}",method = RequestMethod.GET)
 		public List<Task> gettaskbypriority(@PathVariable("priority")int priority)
 		{
 			return taskrepository.gettaskbypriority(priority);
 		}
 		
-		@PutMapping("/updateStatusbyID/{id}/{status}")
+		@RequestMapping(value="/updateStatusbyID/{id}/{status}",method = RequestMethod.PUT)
 		public String updateStatus(@PathVariable ("id")int id,@PathVariable("status")String status)
 		{
 			taskrepository.updateStatus(status, id);
--- taskmanager/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ taskmanager/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -2,17 +2,28 @@ package com.example.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
-
+@EnableJpaRepositories
 @SpringBootApplication
+@EnableSwagger2
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
 	}
-	
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.example.taskmanager")).build();
+	   }
 
 
 }
